d04e8c96b6581e672521ec7120c5e1f414b43ce7
Elthon <eltonsilva.mecatronic@gmail.com>
2022-08-08 22:37:43 -0300

    criando calculo e condicoes do imc

--- imc/script.js
+++ imc/script.js
@@ -10,7 +10,26 @@ function imc() {
 
     if (nome !== "" && altura !== "" && peso !== "") {
         
-        
+        const valorIMC = (peso / (altura * altura)).toFixed(1);
+
+        let classificacao = "";
+
+        if (valorIMC < 18) {
+            classificacao = "abaixo do peso.";
+        }else if (valorIMC < 25) {
+            classificacao = "com peso ideal. Parabéns!";
+        }else if (valorIMC < 30) {
+            classificacao = "levemente acima do peso.";
+        }else if (valorIMC < 35) {
+            classificacao = "com obesidade grau I.";
+        }else if (valorIMC < 40) {
+            classificacao = "com obesidade grau II.";
+        }else {
+            classificacao = "com obesidade grau III. Vai morrer!!!"
+        }
+
+        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}.`
+
     }else {
         resultado.textContent = "Preencha todos os campos!!!"
     }
