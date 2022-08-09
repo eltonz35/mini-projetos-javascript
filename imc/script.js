de939d068479891057b7683c32ae1342603929ee
Elthon <eltonsilva.mecatronic@gmail.com>
2022-08-08 22:02:44 -0300

    criando variaveis calcular e resultado

--- imc/script.js
+++ imc/script.js
@@ -1,6 +1,8 @@
 const nome = document.getElementById("nome");
 const altura = document.getElementById("altura");
 const peso = document.getElementById("peso");
+const calcular = document.getElementById("calcular");
+const resultado = document.getElementById("resultado");
 
 function calcular() {
     
