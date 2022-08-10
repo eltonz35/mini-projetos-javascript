8cf5607bf98fbb87862aefff4c3a7483a06185a9
Elthon <eltonsilva.mecatronic@gmail.com>
2022-08-10 16:45:29 -0300

    criando object turnOn

--- semaforo/semaforo.js
+++ semaforo/semaforo.js
@@ -5,4 +5,10 @@ const trafficLight = () => {
     console.log(event.target.id);
 }
 
+const turnOn = {
+    "red" : () => img.src = "./img/vermelho.png",
+    "yellow" : () => img.src = "./img/amarelo.png",
+    "green" : () => img.src = "./img/verde.png",
+}
+
 buttons.addEventListener("click", trafficLight);
