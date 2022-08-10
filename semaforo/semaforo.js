c7a5cfd63b494243bdd0e5823d48bf3722c1f99d
Elthon <eltonsilva.mecatronic@gmail.com>
2022-08-10 16:38:59 -0300

    criando arrow function

--- semaforo/semaforo.js
+++ semaforo/semaforo.js
@@ -1,3 +1,8 @@
 const img = document.getElementById("img");
 const buttons = document.getElementById("buttons");
 
+const trafficLight = () => {
+    console.log(event.target.id);
+}
+
+buttons.addEventListener("click", trafficLight);
