ccd460fff4b43d36d05650bd9bce259e356a89d1
Elthon <eltonsilva.mecatronic@gmail.com>
2022-08-10 16:53:06 -0300

    aplicando correcao

--- semaforo/semaforo.js
+++ semaforo/semaforo.js
@@ -2,13 +2,13 @@ const img = document.getElementById("img");
 const buttons = document.getElementById("buttons");
 
 const trafficLight = () => {
-    turnOn.red();
+    turnOn[event.target.id]();
 }
 
 const turnOn = {
     "red" : () => img.src = "./img/vermelho.png",
     "yellow" : () => img.src = "./img/amarelo.png",
-    "green" : () => img.src = "./img/verde.png",
+    "green" : () => img.src = "./img/verde.png"
 }
 
 buttons.addEventListener("click", trafficLight);
