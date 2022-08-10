c7a98a3aeee4e220a73be3cb7efa7107ae1a0d43
Elthon <eltonsilva.mecatronic@gmail.com>
2022-08-10 17:07:03 -0300

    criando funcao changColor automatic

--- semaforo/semaforo.js
+++ semaforo/semaforo.js
@@ -5,10 +5,17 @@ const trafficLight = () => {
     turnOn[event.target.id]();
 }
 
+const changeColor = () => {
+    const colors = ["red", "yellow", "green"];
+    const color = colors[colorIndex];
+    turnOn[color]();
+}
+
 const turnOn = {
     "red" : () => img.src = "./img/vermelho.png",
     "yellow" : () => img.src = "./img/amarelo.png",
-    "green" : () => img.src = "./img/verde.png"
+    "green" : () => img.src = "./img/verde.png",
+    "automatic" : () => setInterval(changeColor, 1000)
 }
 
 buttons.addEventListener("click", trafficLight);
