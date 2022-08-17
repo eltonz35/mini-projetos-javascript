07be3c2d577e14a5b59ed77e7de16464db6bbf59
Elthon <eltonsilva.mecatronic@gmail.com>
2022-08-17 16:44:12 -0300

    criando variavel de objetos images

---
+++ slideshow/slideShow.js
@@ -0,0 +1,12 @@
+"use strict"; //instrui o navegador para usar o modo Estrito.
+
+const images = [
+    {"id": "1", "url": "./img/chrono.jpg" },
+    {"id": "2", "url": "./img/inuyasha.jpg" },
+    {"id": "3", "url": "./img/tenchi.jpg" },
+    {"id": "4", "url": "./img/tenjhotenge.jpg" },
+    {"id": "5", "url": "./img/yuyuhakusho.jpg" },
+    {"id": "6", "url": "./img/ippo.jpg" }
+]
+
+loadImagens(images); //Carregamento de imagens
