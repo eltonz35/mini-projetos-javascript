41554be496ac6b2e28e1b3b154e51f005b4cb036
Elthon <eltonsilva.mecatronic@gmail.com>
2022-08-18 15:47:57 -0300

    correcao

--- slideshow/slideShow.js
+++ slideshow/slideShow.js
@@ -6,7 +6,7 @@ const images = [
     {"id": "3", "url": "./img/tenchi.jpg" },
     {"id": "4", "url": "./img/tenjhotenge.jpg" },
     {"id": "5", "url": "./img/yuyuhakusho.jpg" },
-    {"id": "6", "url": "./img/ippo.jpg" }
+    {"id": "6", "url": "./img/ippo.png" }
 ]
 
 const container = document.querySelector("#container-itens");
