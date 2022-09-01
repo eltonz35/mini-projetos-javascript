b496ea8c9c3ea8d6cc15e9f20ed6b471861a26ba
Elthon <eltonsilva.mecatronic@gmail.com>
2022-09-01 16:49:25 -0300

    crian Json sons

--- drumKit/script.js
+++ drumKit/script.js
@@ -1,5 +1,17 @@
 "use strict"
 
+const sons = {
+    "A": "boom.wav",
+    "S": "clap.wav",
+    "D": "hihat.wav",
+    "F": "kick.wav",
+    "G": "openhat.wav",
+    "H": "ride.wav",
+    "J": "snare.wav",
+    "K": "tink.wav",
+    "L": "tom.wav"
+}
+
 const criarDiv = (texto) => {
     const div = document.createElement("div");
     div.classList.add("key");
