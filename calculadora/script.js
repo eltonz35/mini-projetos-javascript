ea8ae51b1b261a7beaf057141f1139e3a7c8547a
Elthon <eltonsilva.mecatronic@gmail.com>
2022-09-06 18:00:44 -0300

    criando funcao que le os numeros

--- calculadora/script.js
+++ calculadora/script.js
@@ -2,3 +2,8 @@
 
 const display = document.getElementById("display");
 const numeros = document.querySelectorAll("[id*=tecla]");
+
+// Função que lê os numeros
+numeros.forEach (numero => 
+    numero.addEventListener("click", inserirNumero)    
+);
